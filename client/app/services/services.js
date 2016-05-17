7bee153163979017e04c06f8363401b539650a7c
Diana <dianajohnson13@gmail.com>
2016-05-16 17:25:19 -0700

    added services file for future requests to server

---
+++ client/app/services/services.js
@@ -0,0 +1,15 @@
+// angular.module('ticTacToe.services', [])
+
+// .factory('UserAccess', function($http) {
+
+//   var login = function(user) {
+//   }
+
+//   var signup = function(user) {
+//   }
+
+//   return {
+//     login: login,
+//     signup: signup
+//   }
+// })
